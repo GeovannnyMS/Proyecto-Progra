1a9ab059320e2ff29e49740ef71a58ce7f653802
GeovannyMS <geovanny.morales.sanchez@est.una.ac.cr>
2025-01-04 09:54:00 -0600

    se cambio el idioma a esoañol

--- primer proyecto de progra/primer proyecto de progra.cpp
+++ primer proyecto de progra/primer proyecto de progra.cpp
@@ -50,74 +50,74 @@ int total[Max_Cursos];
 void RegistroEsrudiantes() {
 	int seleccion = 0;
 	if (contador_estudiaes >= Estudiantes) {
-		cout << "\nSorry we can't rewrite it at this time\n";
+		cout << "\nLo sentimos, no se puede registrar en estos momentos, intentelo mas tarde\n";
 		return;
 	}
-	cout << "Enter your name: ";
+	cout << "Ingrese el nombre del estudiante: ";
 	cin >> Nombres_estudiante[contador_estudiaes];
 
-	cout << "\nEnter your first last name: ";
+	cout << "\nIngrese el primer apellido del estudiante: ";
 	cin >> Primer_apellido_estudiante[contador_estudiaes];
 
-	cout << "\nEnter yout second last name: ";
+	cout << "\nIngrese el segundo apellido del estudiante: ";
 	cin >> Segundo_apellido_estudiante[contador_estudiaes];
 
-	cout << "\nEnter your ID: ";
+	cout << "\nIngrese la cedula del estudiante: ";
 	cin >> Cedulas[contador_estudiaes];
 
 	for (int i = 0; i < contador_estudiaes; i++) {
 		if (Cedulas[i] == Cedulas[i + 1]) {
-			cout << "This student is already registered\n";
+			cout << "El estudiante ya esta registrado\n";
 			return;
 		}
 
 
 	}
 
-	cout << "\nchoose the career\n";
-	cout << "1 = Systems engineering\n2 = English learning\n3 = topography\n4 = administration\nenter the number: ";
+	cout << "\nElija la carrera\n";
+	cout << "1 = Ingenieria En Sistemas\n2 = Aprendisaje Del Ingles\n3 = Topografia\n4 = Administracion\nIngrese El Numero: ";
 	cin >> seleccion;
 	switch (seleccion)
 	{
 	case 1:
-		Carrera[contador_estudiaes] = "Systems engineering";
+		Carrera[contador_estudiaes] = "Ingenieria En Sistemas";
 		break;
 	case 2:
-		Carrera[contador_estudiaes] = "English learning";
+		Carrera[contador_estudiaes] = "Aprendisaje Del Ingles";
 		break;
 	case 3:
-		Carrera[contador_estudiaes] = "topography";
+		Carrera[contador_estudiaes] = "Topografia";
 		break;
 	case 4:
-		Carrera[contador_estudiaes] = "administration";
+		Carrera[contador_estudiaes] = "Administracion";
 		break;
 	}
-	cout << "\nWhat level of the career are you in in numerical format?: ";
+	cout << "\nIngrese el nivel en el que se encuentra el estudiante en forma numerica: ";
 	cin >> nivel[contador_estudiaes];
 
 	cout << endl;
 	contador_estudiaes++;
-	cout << "Registered student\n\n";
+	cout << "Estudiante registrado\n\n";
 
 }
 
 void RegistroCursos() {
 	if (contador_Cursos >= Max_Cursos) {
-		cout << "Cannot add that course\n";
+		cout << "No se puede agregar ese curso\n";
 		return;
 	}
 
-	cout << "Enter the name of the course with the following format\n(Example: Matematica_Para_informatica): ";
+	cout << "Ingrese el nombre de la carrera con el siguiente formato\n(Ejemplo: Matematica_Para_informatica): ";
 	cin >> nombresCursos[contador_Cursos];
-	cout << "\nenter the NCR of the course: ";
+	cout << "\nIngrese el NCR del curso: ";
 	cin >> NCR[contador_Cursos];
-	cout << "\nenter course credits: ";
+	cout << "\nIngrese los creditos del curso: ";
 	cin >> Creditos[contador_Cursos];
-	cout << "\nEnter the teacher's name: ";
+	cout << "\nIngese el nombre del profesor: ";
 	cin >> NombreProfe[contador_Cursos];
-	cout << "\nenter the teacher's first last name: ";
+	cout << "\nIngrese el primer apellido de profesor: ";
 	cin >> PimerApellidoProfe[contador_Cursos];
-	cout << "\nEnter the teacher's second las name: ";
+	cout << "\nIngrese el segundo apellido del profesor: ";
 	cin >> SegundoApellidoProfe[contador_Cursos];
 
 	costoCreditos[contador_Cursos] = Creditos[contador_Cursos] * 1000;
@@ -125,39 +125,39 @@ void RegistroCursos() {
 
 	contador_Cursos++;
 	cout << endl;
-	cout << "added course\n";
+	cout << "Curso registrado\n";
 
 }
 
 void RegistroHorarios() {
 
 	if (contador_Cursos == 0) {
-		cout << "There are no registered courses\n";
+		cout << "No hay cursos registrados\n";
 	}
 
 	int aux;
-	cout << "enter the position to which you want to add the time (1 a " << contador_Cursos << ")\n\n";
+	cout << "A cual curso quiere ponerle el horario (1 a " << contador_Cursos << ")\n\n";
 	for (int i = 0; i < contador_Cursos; i++) {
 		cout << i + 1 << " = " << NCR[i] << "|" << nombresCursos[i] << "|" << Creditos[i] << "|" << NombreProfe[i] << " " << PimerApellidoProfe[i] << " " << SegundoApellidoProfe[i] << endl;
 
 	}
-	cout << "enter the number here: ";
+	cout << "Ingrese el numero del curso a registrar: ";
 	cin >> aux;
 	aux--;
 	if (aux < 0 || aux >= contador_Cursos) {
-		cout << "invalid course\n";
+		cout << "Curso invalido\n";
 		return;
 	}
-	cout << "enter the day: ";
+	cout << "Ingrese el dia de la clase: ";
 	cin >> dias[aux];
-	cout << "\nenter the time (example: 10:00):  ";
+	cout << "\nIngrese la hora de entrda (Ejemplo: 10:00):  ";
 	cin >> HorasEntrada[aux];
-	cout << "\nenter departure time: ";
+	cout << "\nIngrese la hora de salida: ";
 	cin >> HorasSalida[aux];
-	cout << "\nenter the classroom: ";
+	cout << "\nIngrese el aula donde sera la clase: ";
 	cin >> Aunla[aux];
 	cout << endl;
-	cout << "recorded time\n";
+	cout << "Se registro la hora de entrada y salida\n";
 
 }
 
@@ -165,7 +165,7 @@ void SubmenuMatenimiento() {
 	int submenu = 0;
 	do
 	{
-		cout << "what do you need\n\n1 = students\n2 = course\n3 = schedules\n4 = back\n";
+		cout << "¿Que necesita?\n\n1 = Registrar estudiante\n2 = Registrar curso\n3 = Registrar curso\n4 = Atras\n";
 		cin >> submenu;
 
 		switch (submenu)
@@ -190,21 +190,25 @@ void Matricular() {
 	int menu = 0;
 
 	if (contador_estudiaes == 0 || contador_Cursos == 0) {
-		cout << "no students or courses registered\n";
+		cout << "No hay cursos o estudiantes registrados\n";
 		return;
 	}
-	cout << "\nregistered students\n";
+	cout << "\nEstudiantes registrados\n";
 
 	for (int i = 0; i < contador_estudiaes; i++) {
-		cout << i + 1 << " = " << Cedulas[i] << " || " << Nombres_estudiante[i] << "  " << Primer_apellido_estudiante[i] << "  " << Segundo_apellido_estudiante[i] << " " << Carrera[i] << endl;
+		cout << i + 1 << " = " << Cedulas[i] << " || " << Nombres_estudiante[i] << "  " << Primer_apellido_estudiante[i] << "  " << Segundo_apellido_estudiante[i] << " || " << Carrera[i] << endl;
 	}
-	cout << "\nWhich student would you like to enroll?: ";
+	cout << "\nQue estudiante desea matricular cursos?: ";
 	cin >> eleccionE;
 	cout << endl;
 	eleccionE--;
+	total[eleccionE] = 0;
+
+	cout << "\nEl estudiante debe pagar " << total[eleccionE] << " con base a los cursos que ha matriculado\n\n";
+
 
 	if (eleccionE < 0 || eleccionE >= contador_estudiaes) {
-		cout << "invalid student\n";
+		cout << "Estudiante invalido\n";
 		return;
 	}
 	ContadorMatricula[eleccionE] = 0;
@@ -213,11 +217,11 @@ void Matricular() {
 	{
 
 
-		cout << "What course are you going to enroll in?\n";
+		cout << "Que curso quiere matricular\n";
 		for (int i = 0; i < contador_Cursos; i++) {
-			cout << "course " << i + 1 << " || " << NCR[i] << " || " << nombresCursos[i] << endl;
+			cout << "Curso " << i + 1 << " || " << NCR[i] << " || " << nombresCursos[i] << endl;
 		}
-		cout << "\nWhich course do you want to select?: ";
+		cout << "\n¿Qué curso quieres seleccionar?: ";
 		cin >> seleccion;
 		cout << endl;
 		seleccion--;
@@ -225,7 +229,7 @@ void Matricular() {
 		for (int i = 0; i < contador_Cursos; i++) {
 			if (nombresCursos[seleccion] == matricula[eleccionE][i]) {
 
-				cout << "This course is already registered\n";
+				cout << "Este curso ya esta matriculado\n";
 				return;
 			}
 
@@ -238,28 +242,30 @@ void Matricular() {
 
 		total[eleccionE] += costoCreditos[seleccion];
 
-		cout << "Do you want to enroll in more courses?\n1 = Yes\n0 = No\n";
+		cout << "\nEl estudiante debe pagar " << total[eleccionE] << " con base a los cursos que ha matriculado\n\n";
+
+		cout << "Quieres matricular otro curso?\n1 = Si\n0 = No\n";
 		cin >> menu;
 
 	} while (menu != 0);
 
-	cout << "the student has to pay " << total[eleccionE] << " based on the number of credits enrolled\n\n";
+	
 }
 
 void VerEstudiantes() {
 
 	if (contador_estudiaes < 0) {
-		cout << "there are no registered students\n";
+		cout << "No hay estudiantes registrados\n";
 		return;
 	}
-	cout << "enrolled students\n";
+	cout << "Estudiantes registrados\n";
 	for (int i = 0; i < contador_estudiaes; i++) {
 		cout << Cedulas[i] << " || " << Nombres_estudiante[i] << " " << Primer_apellido_estudiante[i] << " " << Segundo_apellido_estudiante[i] << " || " << Carrera[i] << " || level: " << nivel[i] << endl;
 
-		cout << "\nenrolled courses\n\n";
+		cout << "\nCursos matriculados\n\n";
 
 		if (ContadorMatricula[i] == 0) {
-			cout << "This student has not enrolled in any course\n";
+			cout << "El estudiante no ha matriculado ningun curso\n";
 			return;
 		}
 		for (int x = 0; x < contador_Cursos; x++) {
@@ -271,12 +277,12 @@ void VerEstudiantes() {
 }
 void VerCursos() {
 	if (contador_Cursos == 0) {
-		cout << "there are no registered courses\n";
+		cout << "No hay cursos registrados\n";
 		return;
 	}
-	cout << "registered courses\n";
+	cout << "Cursos registrados\n";
 	for (int i = 0; i < contador_Cursos; i++) {
-		cout << "course  " << i + 1 << " || " << NCR[i] << " || " << Creditos[i] << " || " << nombresCursos[i] << endl;
+		cout << "Curso  " << i + 1 << " || " << NCR[i] << " || " << Creditos[i] << " || " << nombresCursos[i] << endl;
 	}
 }
 
@@ -284,7 +290,7 @@ void consulta() {
 	int menu = 0;
 	do
 	{
-		cout << "what you want to consult\n1 = students\n2 = courses\n3 = back\n";
+		cout << "¿Que desea ver?\n1 = Ver estudiantes\n2 = Ver cursos\n3 = Atras\n";
 		cin >> menu;
 		switch (menu)
 		{
@@ -307,7 +313,7 @@ int main()
 
 	do
 	{
-		cout << "Welcome to the UNA registration platform\n1 = Archive\n2 = Maintenance\n3 = Tuition\n4 = Consultation\n0 = Exit\n";
+		cout << "Bienvenido al programa de matricula de la UNA\n1 = Archivo\n2 = Mantenimiento\n3 = Matricula\n4 = Consulta\n0 = Salir\n";
 		cin >> menu;
 
 		switch (menu)
@@ -317,10 +323,11 @@ int main()
 			do
 			{
 
-				cout << "what do you need\n1 = About\n2 = Exit\n";
+				cout << "¿Que desea hace?\n1 = Acerca de\n2 = Salir\n";
 				cin >> submenu;
 				if (submenu == 1) {
-					cout << "\nProgram developer: Geovany Morales Sanchez (Yio)\nID: 604910543\nCareer: Information systems engineering\n\nYio is a very lazy person, it is very difficult for him to feel like doing something but the moment he encounters an exciting challenge his motivation shoots up to the sky and he does not stop studying and trying until he understands it and can assimilate it, that It causes Yio's actions to be labeled as plagiarism because no one has been able to notice that ability to improve silently.\n\n";
+					cout << "\nDesarrollador del programa: Geovany Morales Sanchez (Yio)\nCedula: 604910543\nCarrera: Ingeniería de sistemas de información.\n\nYio es una persona muy vaga, le cuesta mucho tener ganas de hacer algo pero en el momento en que se encuentra con un reto apasionante su motivación se dispara hasta el cielo y no para de estudiar e intentar hasta entenderlo y poder asimilarlo. Eso hace que las acciones de Yio sean etiquetadas como plagio porque nadie ha podido notar esa capacidad de mejorar en silencio..\n";
+					cout << "https://github.com/GeovannnyMS/Proyecto-Progra.git.\n\n";
 				}
 
 
@@ -342,5 +349,6 @@ int main()
 			break;
 		}
 	} while (menu != 0);
-	cout << "Thank you for using the UNA registration systems";
+	cout << "Gracias por usar el sistema de matricula de la UNA\n";
+	cout << "https://github.com/GeovannnyMS/Proyecto-Progra.git";
 }
